24c43204ee1bf00b66ff6cd02f6e3343aad43035
Arnaud Birk <arnaud.birk@ign.fr>
2020-03-26 13:53:23 +0100

    Ajout du client

--- client/client.py
+++ client/client.py
@@ -3,20 +3,25 @@ import requests
 import subprocess
 import json
 import time
+import os
 
 
 if __name__ == "__main__":
 
     print("Demarrage du client GPAO")
 
+    url_api = os.getenv('URL_API', 'localhost')
+
+    print(url_api)
+
     while True:
         print("Recherche d'un nouveau job")
-        req=requests.get('http://api-gpao:8080/api/job/ready')
+        req=requests.get('http://'+url_api+':8080/api/job/ready')
         #print (req.json())
         if(len(req.json())!=0):
             id = req.json()[0]['id']
             command = req.json()[0]['command']
-            
+
             print("L'identifiant du job "+str(id)+" est disponible")
 
             print("Execution de la commande "+ str(command))
@@ -28,10 +33,11 @@ if __name__ == "__main__":
                 status='failed'
                 print("le job a echoue")
 
-            req=requests.post('http://api-gpao:8080/api/job/'+str(id)+'/'+str(status), data=out)
+
+            json_data = out.decode()
+
+            req=requests.post('http://'+url_api+':8080/api/job/'+str(id)+'/'+str(status), json={"log": json_data})
         else:
             print("Aucun job disponible dans la base")
 
         time.sleep(5)
-
-        #break
