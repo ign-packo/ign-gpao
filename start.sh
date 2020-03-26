97bb087751082f91d8254f584d28c5bdbe42be48
Arnaud Birk <arnaud.birk@ign.fr>
2020-03-26 14:21:11 +0100

    Ajout scalabilité du client

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
-docker-compose -f docker-compose.yml up
+NB_PROC=`grep processor /proc/cpuinfo | wc -l`
+NB_PROC=$(( $NB_PROC - 1))
+if [ $NB_PROC -lt 1 ]
+then
+    NB_PROC=1
+fi
+
+echo "Lancement de $NB_PROC thread(s)"
+
+docker-compose -f docker-compose.yml up --scale client-gpao=$NB_PROC
