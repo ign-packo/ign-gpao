ae6d82b0e91c1f7288f363e3837192f0a047cbee
yann-ign <62597411+yann-ign@users.noreply.github.com>
2020-04-16 12:15:35 +0200

    Add files via upload

---
+++ sql/drop_all_4test.sql
@@ -0,0 +1,9 @@
+DROP TABLE dep_jobs_jobs;
+DROP TABLE chantiers;
+DROP TABLE lots;
+DROP TABLE ressources;
+DROP TABLE jobs;
+DROP FUNCTION notify_status_change;
+DROP FUNCTION update_dependency;
+DROP FUNCTION update_job_status;
+
