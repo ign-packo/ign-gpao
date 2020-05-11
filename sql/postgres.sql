4ca6c6b31e70fe41af10339650e81d5d4039450b
Arnaud Birk <arnaud.birk@ign.fr>
2020-05-11 18:19:28 +0200

    Changement type date pour timestamptz

--- sql/postgres.sql
+++ sql/postgres.sql
@@ -345,8 +345,8 @@ ALTER SEQUENCE public.jobdependencies_id_seq OWNED BY public.jobdependencies.id;
 CREATE TABLE public.jobs (
     id integer NOT NULL,
     name character varying NOT NULL,
-    start_date date,
-    end_date date,
+    start_date TIMESTAMPTZ,
+    end_date TIMESTAMPTZ,
     command character varying NOT NULL,
     status public.status DEFAULT 'ready'::public.status NOT NULL,
     return_code integer,
