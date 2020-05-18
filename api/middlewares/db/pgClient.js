7d520a3f74226b4546b94351e2f8dfa17a0fe1e0
gregoire maillet <gregoire.maillet@ign.fr>
2020-05-18 10:59:00 +0200

    fix pgPool + pgClient Error

--- api/middlewares/db/pgClient.js
+++ api/middlewares/db/pgClient.js
@@ -1,5 +1,5 @@
 const debug = require('debug')('pgClient');
-const { Pool } = require('pg');
+const { Client } = require('pg');
 
 /*
  * middleware pour la création et la libération des connexions postgresql
@@ -7,30 +7,46 @@ const { Pool } = require('pg');
 
 async function open(req, res, next) {
   debug('open pg connection...');
-  req.pgPool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
-  });
-  // Debut de la transaction
-  await req.pgPool.query('BEGIN');
-
-  next();
+  try {
+    req.client = new Client({
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASE,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT,
+    });
+    await req.client.connect();
+    await req.client.query('BEGIN');
+    next();
+  } catch (error) {
+    debug(error);
+    req.error = {
+      msg: error.toString(),
+      code: 500,
+      function: 'pgClient open',
+    };
+    res.status(req.error.code).json(req.error);
+  }
 }
 
 async function close(req, res, next) {
   debug('close pg connection...');
-  if (req.error) {
-    debug('rollback');
-    await req.pgPool.query('ROLLBACK');
-  } else {
-    debug('commit');
-    await req.pgPool.query('COMMIT');
+  try {
+    if (req.error) {
+      debug('rollback');
+      await req.client.query('ROLLBACK');
+    } else {
+      debug('commit');
+      await req.client.query('COMMIT');
+    }
+  } catch (error) {
+    req.error = {
+      msg: error.toString(),
+      code: 500,
+      function: 'pgClient close commit',
+    };
   }
-  req.pgPool.end();
-
+  req.client.end();
   next();
 }
 
