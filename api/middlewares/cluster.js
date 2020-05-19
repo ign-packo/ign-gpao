a794458f4a86fa150c2dc831a9b6f98b79f84c94
gregoire maillet <gregoire.maillet@ign.fr>
2020-05-19 14:55:40 +0200

    ajout d une route unavailable sur les clusters

--- api/middlewares/cluster.js
+++ api/middlewares/cluster.js
@@ -1,4 +1,5 @@
 const { matchedData } = require('express-validator/filter');
+const debug = require('debug')('job');
 
 async function getAllClusters(req, res, next) {
   await req.client.query('SELECT * FROM cluster')
@@ -19,8 +20,18 @@ async function insertCluster(req, res, next) {
 
   const { host } = params;
 
-  await req.client.query(
-    'INSERT INTO cluster (host, id_thread, active, available) VALUES ( $1 , (select count(id) from cluster where host = $2), true, true ) RETURNING id',
+  await req.pgPool.query(
+    'LOCK TABLE cluster IN EXCLUSIVE MODE',
+  )
+    .catch((error) => {
+      req.error = {
+        msg: error.toString(),
+        code: 500,
+        function: 'insertCluster',
+      };
+    });
+  await req.pgPool.query(
+    'INSERT INTO cluster (host, id_thread, active, available) VALUES ( $1 , (select count(id) from cluster where host = $2 AND available = \'True\'), true, true ) RETURNING id',
     [host, host],
   )
     .then((results) => { req.result = results.rows; })
@@ -34,7 +45,23 @@ async function insertCluster(req, res, next) {
   next();
 }
 
+async function unavailableCluster(req, res, next) {
+  const params = matchedData(req);
+  const { id } = params;
+  debug('update ', id);
+  await req.pgPool.query('UPDATE cluster SET available=\'False\' WHERE id=$1', [id])
+    .catch((error) => {
+      req.error = {
+        msg: error.toString(),
+        code: 500,
+        function: 'updateCluster',
+      };
+    });
+  next();
+}
+
 module.exports = {
   getAllClusters,
   insertCluster,
+  unavailableCluster,
 };
