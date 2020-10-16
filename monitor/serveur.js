b84f5197cc0f7f42c169f32a0cbd6b13e2581520
Arnaud Birk <arnaud.birk@ign.fr>
2020-10-16 16:42:13 +0200

    Clarification de la variable SERVER renommé en SERVER_HOSTNAME

--- monitor/serveur.js
+++ monitor/serveur.js
@@ -3,14 +3,22 @@ const debug = require('debug');
 
 const app = express();
 
+// Url du moniteur sur lequel fonctionne le moniteur
+// par défaut http://localhost:8000
 const URL_MONITOR = process.env.URL_MONITOR || 'localhost';
 const MONITOR_PORT = process.env.MONITOR_PORT || 8000;
 
+// Url de l'api qui est appelé par le moniteur
+// par défaut http://localhost:8080
 const URL_API = process.env.URL_API || 'localhost';
 const API_PORT = process.env.API_PORT || 8080;
 
-const SERVER = process.env.SERVER || 'localhost';
-const SERVER_URL = `http://${SERVER}:${API_PORT}`;
+// Dans le cas d'un déploiement de la stack via docker les variables d'environnement URL_MONITOR, MONITOR_PORT, URL_API, API_PORT
+// sont surchargées dans le docker-compose.yml.
+
+// Nom du serveur d'un point de vue client nécessaires pour les requêtes vers l'api executé côté client
+const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
+const SERVER_URL = `http://${SERVER_HOSTNAME}:${API_PORT}`;
 
 app.set('apiUrl', `http://${URL_API}:${API_PORT}`);
 app.set('apiMonitor', `http://${URL_MONITOR}:${MONITOR_PORT}`);
