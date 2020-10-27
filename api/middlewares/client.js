1d5835e27440f6a139822f4ff9be8ef40543a1af
Arnaud Birk <arnaud.birk@ign.fr>
2020-10-27 12:19:06 +0100

    Ajout route créant un zip avec le client et ajout du lien dans la doc

---
+++ api/middlewares/client.js
@@ -0,0 +1,37 @@
+const debug = require('debug')('client');
+const AdmZip = require('adm-zip');
+
+async function getClient(req, res, next) {
+    debug('getClient');
+
+    const zip = new AdmZip();
+    debug(`dirname = ${__dirname}`);
+    zip.addLocalFile(`${__dirname}/../../client/client.py`);
+    zip.addLocalFile(`${__dirname}/../../client/start.sh`);
+
+    // AB : A decommenter lorsque ce fichier sera dans le dépôt
+    //zip.addLocalFile(`${__dirname}/../../client/start.bat`);
+
+    // AB : On ajoute un fichier de version pour pouvoir faire un 
+    // contrôle sur les version au lacement du client ?
+
+    const data = zip.toBuffer();
+    const zipName = 'client.zip';
+
+    zip.writeZip(`${zipName}`);
+
+    debug(`Zip : ${__dirname}/../${zipName}`);
+
+    res.setHeader('Content-Type','application/octet-stream');
+    res.setHeader('Content-Disposition',`attachment; filename=${zipName}`);
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Description,content-disposition');
+    res.setHeader('Content-Length',data.length);
+    res.send(data);
+    debug("Fin getClient");
+
+    next();
+  }
+
+module.exports = {
+    getClient,
+  };
