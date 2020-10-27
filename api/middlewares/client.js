20274e9450540eae837c79205ccaee17ec0bc04f
Arnaud Birk <arnaud.birk@ign.fr>
2020-10-27 14:19:18 +0100

    fix lint

--- api/middlewares/client.js
+++ api/middlewares/client.js
@@ -2,36 +2,36 @@ const debug = require('debug')('client');
 const AdmZip = require('adm-zip');
 
 async function getClient(req, res, next) {
-    debug('getClient');
+  debug('getClient');
 
-    const zip = new AdmZip();
-    debug(`dirname = ${__dirname}`);
-    zip.addLocalFile(`${__dirname}/../../client/client.py`);
-    zip.addLocalFile(`${__dirname}/../../client/start.sh`);
+  const zip = new AdmZip();
+  debug(`dirname = ${__dirname}`);
+  zip.addLocalFile(`${__dirname}/../../client/client.py`);
+  zip.addLocalFile(`${__dirname}/../../client/start.sh`);
 
-    // AB : A decommenter lorsque ce fichier sera dans le dépôt
-    //zip.addLocalFile(`${__dirname}/../../client/start.bat`);
+  // AB : A decommenter lorsque ce fichier sera dans le dépôt
+  // zip.addLocalFile(`${__dirname}/../../client/start.bat`);
 
-    // AB : On ajoute un fichier de version pour pouvoir faire un 
-    // contrôle sur les version au lacement du client ?
+  // AB : On ajoute un fichier de version pour pouvoir faire un
+  // contrôle sur les version au lacement du client ?
 
-    const data = zip.toBuffer();
-    const zipName = 'client.zip';
+  const data = zip.toBuffer();
+  const zipName = 'client.zip';
 
-    zip.writeZip(`${zipName}`);
+  zip.writeZip(`${zipName}`);
 
-    debug(`Zip : ${__dirname}/../${zipName}`);
+  debug(`Zip : ${__dirname}/../${zipName}`);
 
-    res.setHeader('Content-Type','application/octet-stream');
-    res.setHeader('Content-Disposition',`attachment; filename=${zipName}`);
-    res.setHeader('Access-Control-Expose-Headers', 'Content-Description,content-disposition');
-    res.setHeader('Content-Length',data.length);
-    res.send(data);
-    debug("Fin getClient");
+  res.setHeader('Content-Type', 'application/octet-stream');
+  res.setHeader('Content-Disposition', `attachment; filename=${zipName}`);
+  res.setHeader('Access-Control-Expose-Headers', 'Content-Description,content-disposition');
+  res.setHeader('Content-Length', data.length);
+  res.send(data);
+  debug('Fin getClient');
 
-    next();
-  }
+  next();
+}
 
 module.exports = {
-    getClient,
-  };
+  getClient,
+};
