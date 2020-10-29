9287b7d092db22ce0d954fdd6e049c0a0a33233f
Arnaud Birk <arnaud.birk@ign.fr>
2020-10-29 14:24:21 +0100

    bricole proxy + modification docker

--- build-image.sh
+++ build-image.sh
@@ -1,1 +1,3 @@
-docker-compose build --parallel --no-cache --force-rm
+export SERVER_HOSTNAME=$HOSTNAME
+
+docker-compose build --build-arg http_proxy=${http_proxy} --build-arg https_proxy=${https_proxy} --parallel --no-cache --force-rm
