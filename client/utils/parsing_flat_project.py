a12adec86ffd5558e15496a95dcc48d0177c00a8
Yann Le-Borgne <YLe-Borgne@ign.fr>
2020-11-25 18:40:48 +0100

    Gestion des erreurs: creation d'un fichier d'erreur, indexation des erreurs, check unicité des id, existance des id dans les deps.. etc

--- client/utils/parsing_flat_project.py
+++ client/utils/parsing_flat_project.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import unittest
 import re
 import json
@@ -18,6 +19,7 @@ comma=','
 
 kw_name="name"
 return_code=0
+errFile=None
 
 
 
@@ -73,6 +75,61 @@ def print_help():
     print(f"\tinputfile.flat: Translates a project \"my_project.flat\" \n\t\t\tinto \"my_project.json\".")
     print(f"-----------------------------------------------------------------------------")
 
+
+def check_idName(data):
+    global return_code
+    # print(f"{json.dumps(data, indent=2)}")
+
+    projects_dict={}   
+    index_project=0
+    #Init dict with project id and rank {key=id_project, value=rank}
+    for key_project in data.keys():
+        projects_dict[key_project]=index_project
+        index_project+=1
+
+    for key_project in data.keys():
+
+        jobs_dict={}
+        index_job=0
+        #Init dict with job id and rank {key=id_job, value=rank}
+        for key_job in data[key_project]['JOBS'].keys():
+            jobs_dict[key_job]=index_job
+            index_job+=1
+
+        for key_job in data[key_project]['JOBS'].keys():
+            index_depjob=0
+            for dep_job in data[key_project]['JOBS'][key_job]['DEP_JOBS']:
+                try:
+                    #Exit? test by jobs_dict[dep_job['id_job']]
+                    #If NOT Exit replace id_name by rank else exception
+                    data[key_project]['JOBS'][key_job]['DEP_JOBS'][index_depjob]['id_job']=jobs_dict[dep_job['id_job']]
+                    
+                except KeyError as e:
+                    errFile.write(f"DEPENDENCY ERROR (line:{dep_job['NUMLINE']}): jobs \"{dep_job['id_job']}\" doesn't exist\n")                  
+                    return_code=1
+
+                index_depjob+=1
+        
+        index_depproject=0   
+        for dep_project in data[key_project]['DEP_PROJECTS']:
+            #print(f"{dep_project}")
+            try:
+                #Exit? test by jobs_dict[dep_job['id_job']]
+                #If NOT Exit replace id_name by rank else exception
+                data[key_project]['DEP_PROJECTS'][index_depproject]['id_project']=projects_dict[dep_project['id_project']]
+            except KeyError as e:
+                    errFile.write(f"DEPENDENCY ERROR (line:{dep_project['NUMLINE']}): project \"{dep_project['id_project']}\" doesn't exist\n")
+                    return_code=1
+
+            index_depproject+=1   
+        
+    #print(f"---{projects_dict}")
+    #print(f"---{jobs_dict}")
+
+    #print(f"{json.dumps(data, indent=2)}") 
+
+    return 
+    
 def switch_idName_id(data_f, data_dict):
     for v in data_dict:
         if 'deps' in v.keys():
@@ -147,38 +204,54 @@ def get_lenght(data_dict):return len(data_dict)
 def get_last_index (data_dict):return len(data_dict)-1
 
 
-def read_next(key, match, file_object, data):
-    #print('--> in read_next')
-    global parent
-    if key=='PROJECT':
-        data[match.group('p_name')] = {"COM": (match.group('p_comment')), "JOBS":{}, "DEP_PROJECTS":[]}
+def read_next(numline, file_object, data):
+    global return_code
+    #line = "\n"
+    line = file_object.readline()
 
-    elif key=='JOB':
-        data[get_last_key(data)]['JOBS'][match.group('j_name')]={"CMD": (match.group('j_command')), "DEP_JOBS":[] }
-
-    elif key == 'DEP_JOB':
-        data[get_last_key(data)]['JOBS'][get_last_key(data[get_last_key(data)]['JOBS'])]['DEP_JOBS'].append(match.group('j_name'))
-
-    elif key == 'DEP_PROJECT':
-        data[get_last_key(data)]['DEP_PROJECTS'].append(match.group('p_name'))
-    
-    line = "\n"
     while line.isspace(): 
         line = file_object.readline()
+        numline+=1
 
     key, match = parse_line(line)
-   
-    if key != None: read_next(key, match, file_object, data)
+
+    if key=='PROJECT':
+        if match.group('p_name') in data :
+            errFile.write(f"PROJECT ERROR, DUPLICATE NAME(line:{numline}): project \"{match.group('p_name')}\" already exist\n")
+            return_code=1
+        else:
+            data[match.group('p_name')] = {"COM": (match.group('p_comment')), "JOBS":{}, "DEP_PROJECTS":[], "NUMLINE":numline}
+
+    elif key=='JOB':
+        if match.group('j_name') in data[get_last_key(data)]['JOBS'] :
+            errFile.write(f"JOB ERROR, DUPLICATE NAME(line:{numline}): job \"{match.group('j_name')}\" already exist\n")
+            return_code=1
+        else:
+            data[get_last_key(data)]['JOBS'][match.group('j_name')]={"CMD": (match.group('j_command')), "DEP_JOBS":[], "NUMLINE":numline }
+
+    elif key == 'DEP_JOB':
+        data[get_last_key(data)]['JOBS'][get_last_key(data[get_last_key(data)]['JOBS'])]['DEP_JOBS'].append({ "id_job":match.group('j_name'), "NUMLINE":numline})
+        #data[get_last_key(data)]['JOBS'][get_last_key(data[get_last_key(data)]['JOBS'])]['DEP_JOBS'].append(numline)
+
+    elif key == 'DEP_PROJECT':
+        #data[get_last_key(data)]['DEP_PROJECTS'].append(match.group('p_name'))
+        data[get_last_key(data)]['DEP_PROJECTS'].append({"id_project":match.group('p_name'), "NUMLINE":numline})
+    
+    numline+=1
+    if key != None: read_next(numline, file_object, data)
     #print('--> out read_next') 
 
 
 def write_JSON4GPAO(data, data_json):
     data_json['PROJECTS']=[]
+
     df_p=pan.DataFrame({'C_TARGET':{}})
     i_p=0
+
     for key_project, values in data['PROJECTS'].items():
         dict_project={}
         dict_project['name']=key_project
+
         df_p.loc[key_project,'C_TARGET']=i_p
         i_p+=1
 
@@ -200,12 +273,13 @@ def write_JSON4GPAO(data, data_json):
                 i=0
                 while i < len_dep_jobs:
                     dict_dep_job={}
-                    dict_dep_job['id']=data['PROJECTS'][key_project]['JOBS'][key_job]['DEP_JOBS'][i]
+                    dict_dep_job['id']=data['PROJECTS'][key_project]['JOBS'][key_job]['DEP_JOBS'][i]['id_job']
                     dict_job['deps'].append(dict_dep_job)
                     i+=1
 
             dict_project['jobs'].append(dict_job)
-        switch_idName_id(df_j, dict_project['jobs'])
+
+        #switch_idName_id(df_j, dict_project['jobs'])
         # End for key_job in.....
 
         len_dep_projects=len(data['PROJECTS'][key_project]['DEP_PROJECTS'])
@@ -214,41 +288,57 @@ def write_JSON4GPAO(data, data_json):
             i=0
             while i < len_dep_projects:
                 dict_dep_project={}
-                dict_dep_project['id']=data['PROJECTS'][key_project]['DEP_PROJECTS'][i]
+                dict_dep_project['id']=data['PROJECTS'][key_project]['DEP_PROJECTS'][i]['id_project']
                 dict_project['deps'].append(dict_dep_project)
                 i+=1
 
         data_json['PROJECTS'].append(dict_project)
     # end for key_project.......
-    switch_idName_id(df_p, data_json['PROJECTS'])
+    #switch_idName_id(df_p, data_json['PROJECTS'])
     return data_json
     
         
 		
 def parse_flat_file(infile):
     data={"PROJECTS":{}} 
+    numline=1
     with open(infile, 'r') as file_object:
-        dict_data=data['PROJECTS']
-        line = file_object.readline()
-        if not line.isspace():
-            key, match = parse_line(line)
-            read_next(key, match, file_object, dict_data)
-    data_json = {"PROJECTS":[]} 
-    data_json = write_JSON4GPAO(data, data_json)
-    return data_json
+        projects_dict=data['PROJECTS']    
+        read_next(numline, file_object, projects_dict)
+    
+    check_idName(projects_dict)
+
+    # print(f"{json.dumps(projects_dict, indent=2)}")
+    # exit (1)
+
+    if return_code==0:
+        data_json = {"PROJECTS":[]} 
+        data_json = write_JSON4GPAO(data, data_json)
+        return data_json
+    else :
+        return None
+    
        
 def parse_json_file(infile):
     print(f'function not implented yet.......')
     return None
 
-def parse_file(infile, extend):
-    if in_extend[1]=="flat" :
+def parse_file(infile):
+    a_infile=infile.split('.')
+    errFileName=a_infile[0]+".err"
+    if os.path.exists(errFileName):
+        os.remove(errFileName)
+    global errFile
+    errFile = open(errFileName, "x")
+
+    if a_infile[1]=="flat" :
         dj=parse_flat_file(in_filepath)
-    elif in_extend[1]=="json":
+    elif a_infile[1]=="json":
         dj=parse_json_file(in_filepath)
     else:
         dj=None 
-        print(f"Warning: [{extend}] unprocessed file type")
+        print(f"WARNING: [{a_infile[1]}] unprocessed file type")
+        exit(1)
 
     return dj
 
@@ -262,14 +352,14 @@ if __name__ == '__main__':
         generate_empty_project()
     elif sys.argv[1]=='print':
         in_filepath = sys.argv[2]
-        in_extend=in_filepath.split(".")
-        dj=parse_file(in_filepath,in_extend[1])
-        mstr=json.dumps(dj, indent=2)
-        print(f"{mstr}")
+        dj=parse_file(in_filepath)
+
+        if dj!=None:
+            mstr=json.dumps(dj, indent=2)
+            print(f"{mstr}")
     else:
         in_filepath = sys.argv[1]
-        in_extend=in_filepath.split(".")
-        dj=parse_file(in_filepath,in_extend[1])
+        dj=parse_file(in_filepath)
 
         if dj != None:
             out_filepath = sys.argv[2]
@@ -278,6 +368,6 @@ if __name__ == '__main__':
 
      
     if return_code !=0:
-        print(f"ENDED WITH ERROR(s) see output screen or file!!!!")
+        print(f"ENDED WITH ERROR(s) see output file errors !!!!")
     else :
-        print(f"ENDED WITH NO ERROR(s)")   
+        print(f"ENDED WITH NO ERROR")   
