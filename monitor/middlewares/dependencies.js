6ef53941d292ea7aefbb2eee617aab33aa8f54d0
greg maillet <gregoire.maillet@ign.fr>
2021-06-03 22:21:31 +0200

    perf(monitor): amelioration des performances sur les gros chantiers

--- monitor/middlewares/dependencies.js
+++ monitor/middlewares/dependencies.js
@@ -3,6 +3,29 @@ const axios = require('axios');
 async function getDependencies(req, res, next) {
   const json = await axios.get(`${req.app.get('apiUrl')}/api/dependencies?id_job=${req.params.id}`);
 
+  req.dependencies_data = JSON.stringify(json.data);
+  req.dependencies_columns = JSON.stringify([
+    {
+      title: 'Id',
+      data: 'dep_id',
+    },
+    {
+      title: 'Job en amont',
+      data: 'dep_up',
+    },
+    {
+      title: 'Nom du job',
+      data: 'job_name',
+    },
+    {
+      title: 'Statut',
+      data: 'job_status',
+    },
+    {
+      title: 'Active',
+      data: 'dep_active',
+    },
+  ]);
   req.deps = json.data;
   next();
 }
