12ec9464654ff5d1309eb256dc939835259080fe
Arnaud Birk <arnaud.birk@ign.fr>
2021-12-03 12:01:33 +0100

    fix(Client): best error management and console log

--- client/client.py
+++ client/client.py
@@ -16,7 +16,20 @@ import shlex
 import platform
 import ctypes
 import argparse
+import logging
 import requests
+
+HOSTNAME = socket.gethostname()
+NB_PROCESS = multiprocessing.cpu_count()
+
+logging.basicConfig(
+    handlers=[
+        logging.FileHandler("client.log"),
+        logging.StreamHandler()
+    ],
+    format='%(asctime)s %(levelname)-5s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 URL_API = (
     "http://"
@@ -29,6 +42,37 @@ URL_API = (
 MIN_AVAILABLE_SPACE = 5
 # duree minimale en s entre deux requetes de mise a jour du log
 MIN_FLUSH_RATE = 5
+
+
+def send_request(url, mode, json=None, str_thread_id=None):
+    """ Fonction executant les requetes http """
+    success = False
+    logging.debug("%s : %s : %s%s", str_thread_id, mode, URL_API, url)
+    while not success:
+        try:
+            if mode == "GET":
+                req = requests.get(URL_API+url)
+                req.raise_for_status()
+                return req
+            if mode == "PUT":
+                req = requests.put(URL_API+url)
+                req.raise_for_status()
+                return req
+            if mode == "POST":
+                req = requests.post(URL_API+url, json=json)
+                req.raise_for_status()
+                return req
+        except requests.exceptions.Timeout:
+            logging.error("%s : timeout sur l'url : %s", str_thread_id, url)
+            time.sleep(1)
+        except requests.exceptions.TooManyRedirects:
+            logging.error("%s : timeout sur l'url : %s", str_thread_id, url)
+            time.sleep(1)
+        except requests.exceptions.RequestException as exception:
+            logging.error("%s : Erreur sur la requete : %s",
+                          str_thread_id, url)
+            logging.error("%s : Erreur %s", str_thread_id, exception)
+            time.sleep(1)
 
 
 def get_free_space_gb(dirname):
@@ -47,7 +91,7 @@ def get_free_space_gb(dirname):
     return space_available
 
 
-def read_stdout_process(proc, id_job):
+def read_stdout_process(proc, id_job, str_thread_id):
     """ Lecture de la sortie console """
     last_flush = time.time()
     realtime_output = ""
@@ -58,21 +102,24 @@ def read_stdout_process(proc, id_job):
             # entre temps, des nouveaux messages sont peut-etre arrives
             for line in proc.stdout.readlines():
                 realtime_output += line
-            if len(realtime_output) > 0:
-                requests.post(
-                    URL_API + "job/" + str(id_job) + "/appendLog",
-                    json={"log": realtime_output},
-                )
+
+            if realtime_output:
+                url_tmp = "job/" + str(id_job) + "/appendLog"
+
+                send_request(url_tmp,
+                             "POST",
+                             json={"log": realtime_output},
+                             str_thread_id=str_thread_id)
             break
 
-        if (
-            len(realtime_output) > 0
-            and (time.time() - last_flush) > MIN_FLUSH_RATE
-        ):
-            requests.post(
-                URL_API + "job/" + str(id_job) + "/appendLog",
-                json={"log": realtime_output},
-            )
+        if (realtime_output and (time.time() - last_flush) > MIN_FLUSH_RATE):
+            url_tmp = "job/" + str(id_job) + "/appendLog"
+
+            send_request(url_tmp,
+                         "POST",
+                         json={"log": realtime_output},
+                         str_thread_id=str_thread_id)
+
             realtime_output = ""
             last_flush = time.time()
 
@@ -81,15 +128,9 @@ def launch_command(job, str_thread_id, shell, working_dir):
     """ Lancement d'une ligne de commande """
     id_job = job["id"]
     command = job["command"]
-    print(
-        str_thread_id,
-        "L'identifiant du job "
-        + str(id_job)
-        + " est disponible"
-        + " Execution de la commande ["
-        + str(command)
-        + "]",
-    )
+    logging.info("%s L'id du job %s est disponible. "
+                 "Execution de la commande [%s]",
+                 str_thread_id, id_job, command)
     return_code = None
     error_message = ""
     try:
@@ -105,7 +146,7 @@ def launch_command(job, str_thread_id, shell, working_dir):
             universal_newlines=True,
             cwd=working_dir,
         ) as proc:
-            read_stdout_process(proc, id_job)
+            read_stdout_process(proc, id_job, str_thread_id)
             return_code = proc.poll()
             status = "done"
     except subprocess.CalledProcessError as ex:
@@ -119,17 +160,15 @@ def launch_command(job, str_thread_id, shell, working_dir):
         if return_code is None:
             return_code = -1
     if error_message:
-        print("Erreur : " + error_message)
+        logging.error("Erreur : %s", error_message)
     error_message += "FIN"
     return id_job, return_code, status, error_message
 
 
-def process(param):
+def process(parameters):
     """ Traitement pour un thread """
-    thread_id = param[0]
-    hostname = param[1]
-    tags = param[2]
-    str_thread_id = "[" + str(thread_id) + "] : "
+    str_thread_id = "[" + str(parameters[0]) + "]"
+    tags = parameters[1]
     id_session = -1
     # AB : Il faut passer shell=True sous windows
     # pour que les commandes systemes soient reconnues
@@ -140,37 +179,30 @@ def process(param):
         # courant qui devient le dossier d'execution
         with tempfile.TemporaryDirectory(dir=".") as working_dir:
 
-            url = URL_API + "session?host=" + hostname
+            url = "session?host=" + HOSTNAME
             if tags:
-                url += "&tags=" + tags
+                url += id_session + "&tags=" + tags
 
-            print(url)
+            req = send_request(url, "PUT", str_thread_id=str_thread_id)
 
-            req = requests.put(url)
             id_session = req.json()[0]["id"]
-            print(
-                str_thread_id
-                + " : working dir ("
-                + working_dir
-                + ") id_session ("
-                + str(id_session)
-                + ")"
-            )
+            logging.info("%s : working dir (%s) id_session (%s)",
+                         str_thread_id, working_dir, id_session)
             while True:
                 # on verifie l'espace disponible dans le dossier de travail
                 free_gb = get_free_space_gb(working_dir)
                 req = None
                 if free_gb < MIN_AVAILABLE_SPACE:
-                    print(
-                        "espace disque insuffisant : ",
+                    logging.warning(
+                        "Espace disque insuffisant : %s/%s",
                         free_gb,
-                        "/",
-                        MIN_AVAILABLE_SPACE,
-                    )
+                        MIN_AVAILABLE_SPACE
+                        )
                 else:
-                    req = requests.get(
-                        URL_API + "job/ready?id_session=" + str(id_session)
-                    )
+                    url_tmp = "job/ready?id_session=" + str(id_session)
+                    req = send_request(url_tmp,
+                                       "GET",
+                                       str_thread_id=str_thread_id)
                 if req and req.json():
                     (
                         id_job,
@@ -180,36 +212,46 @@ def process(param):
                     ) = launch_command(
                         req.json()[0], str_thread_id, shell, working_dir
                     )
-                    print("Mise a jour : ", return_code, status, error_message)
-                    req = requests.post(
-                        URL_API
-                        + "job?id="
-                        + str(id_job)
-                        + "&status="
-                        + str(status)
-                        + "&returnCode="
-                        + str(return_code),
-                        json={"log": error_message},
-                    )
+
+                    logging.info("%s Maj du job: %s, code_retour: %s, "
+                                 "status : %s, error : %s",
+                                 str_thread_id,
+                                 id_job, return_code,
+                                 status,
+                                 error_message)
+
+                    url_tmp = ("job?id=" + str(id_job) +
+                               "&status=" + str(status) +
+                               "&returnCode=" + str(return_code))
+
+                    req = send_request(url_tmp,
+                                       "POST",
+                                       json={"log": error_message},
+                                       str_thread_id=str_thread_id)
+
                     if req.status_code != 200:
-                        print("Error : ", req.status_code, req.content)
+                        logging.error("%s Mauvais statut code : %s, %s",
+                                      str_thread_id,
+                                      req.status_code,
+                                      req.content)
 
                 time.sleep(random.randrange(10))
     except KeyboardInterrupt:
-        print("on demande au process de s'arreter")
-        req = requests.post(URL_API + "session/close?id=" + str(id_session))
-    print(str_thread_id, "end thread ")
+        logging.info("%s : on demande au process de s'arreter", str_thread_id)
+
+        req = send_request("session/close?id=" + str(id_session),
+                           "POST",
+                           str_thread_id=str_thread_id)
+
+    logging.info("%s : Fin du thread", str_thread_id)
 
 
 if __name__ == "__main__":
-    HOSTNAME = socket.gethostname()
-    NB_PROCESS = multiprocessing.cpu_count()
+    logging.info("Demarrage du client GPAO")
+    logging.info("URL_API : %s", URL_API)
 
-    print("Demarrage du client GPAO")
-    print("URL_API : " + URL_API)
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument(
         "-n",
         "--threads",
         required=False,
@@ -217,7 +259,7 @@ if __name__ == "__main__":
         help="fix the number of threads \
                         (default: estimated number of cpu on the system)",
     )
-    parser.add_argument(
+    PARSER.add_argument(
         "-s",
         "--suffix",
         help="add a suffix on the hostname \
@@ -226,7 +268,7 @@ if __name__ == "__main__":
         required=False,
         type=str,
     )
-    parser.add_argument(
+    PARSER.add_argument(
         "-t",
         "--tags",
         required=False,
@@ -234,24 +276,26 @@ if __name__ == "__main__":
         default="",
         help="comma separated list of tags",
     )
-    args = parser.parse_args()
+    ARGS = PARSER.parse_args()
 
-    print(args)
-    if args.threads:
-        if args.threads <= 0:
-            print("Le nombre de thread doit être >0 : ", args.threads)
+    logging.debug(ARGS)
+    if ARGS.threads:
+        if ARGS.threads <= 0:
+            logging.error("Le nombre de thread doit etre >0 : %s",
+                          ARGS.threads)
             sys.exit(1)
-        NB_PROCESS = args.threads
-    if args.suffix:
-        HOSTNAME += args.suffix
+        NB_PROCESS = ARGS.threads
+    if ARGS.suffix:
+        HOSTNAME += ARGS.suffix
 
-    print("HOSTNAME : ", HOSTNAME)
-    print("NB_PROCESS : ", NB_PROCESS)
+    logging.info("HOSTNAME : %s", HOSTNAME)
+    logging.info("NB_PROCESS : %s", NB_PROCESS)
 
-    req_nb_sessions = requests.get(URL_API + "nodes")
-    nodes = req_nb_sessions.json()
+    REQ_NB_SESSIONS = send_request("nodes", "GET")
+
+    NODES = REQ_NB_SESSIONS.json()
     NB_SESSION = 0
-    for node in nodes:
+    for node in NODES:
         if node["host"] == HOSTNAME:
             # attention, les donnees sont en string
             # a corriger dans l'API
@@ -259,32 +303,29 @@ if __name__ == "__main__":
                 int(node["active"]) + int(node["idle"]) + int(node["running"])
             )
     if NB_SESSION > 0:
-        print(
-            "Erreur: il y a déjà des sessions "
-            "ouvertes avec ce nom de machine."
-        )
-        print(
-            "Pour lancer plusieurs client sur une même machine, "
-            "utiliser un suffixe (ex: python client.py -s _MonSuffixe)."
-        )
+        logging.error("Erreur: il y a deja des sessions "
+                      "ouvertes avec ce nom de machine.")
+        logging.error("Pour lancer plusieurs client sur une meme machine,"
+                      " utiliser un suffixe "
+                      "(ex: python client.py -s _MonSuffixe).")
         sys.exit(1)
 
     with multiprocessing.Pool(NB_PROCESS) as POOL:
         signal.signal(signal.SIGINT, signal.SIG_IGN)
 
-        parameters = []
-        for thread_number in range(NB_PROCESS):
-            parameters.append((thread_number, HOSTNAME, args.tags))
+        PARAMETERS = []
+        for id_thread in range(NB_PROCESS):
+            PARAMETERS.append((id_thread, ARGS.tags))
 
         try:
-            POOL.map(process, parameters)
+            POOL.map(process, PARAMETERS)
 
         except KeyboardInterrupt:
-            print("on demande au pool de s'arreter")
+            logging.info("on demande au pool de s'arreter")
             POOL.terminate()
         else:
-            print("Normal termination")
+            logging.info("Normal termination")
             POOL.close()
         POOL.join()
 
-    print("Fin du client GPAO")
+    logging.info("Fin du client GPAO")
